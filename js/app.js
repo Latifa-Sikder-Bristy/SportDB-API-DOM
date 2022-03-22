3f014bca9c65ee4f4533c764eb9b0d1641379af5
Latifa Sikder Bristy <latifasikderbristy@gmail.com>
2022-03-22 10:19:16 +0600

    half commit

---
+++ js/app.js
@@ -0,0 +1,44 @@
+const allplayers = () => { 
+    const searchValue = document.getElementById("search-box").value;
+    const url = `https://www.thesportsdb.com/api/v1/json/2/searchplayers.php?p=${searchValue}`;
+    // console.log(url);
+    fetch(url)
+    .then((response) => response.json())
+    .then((data) => showPlayerDetails(data.player));
+    
+};
+
+const showPlayerDetails = (players) => {
+    for (const player of players) {
+        const parent = document.getElementById ("player-container");
+        const div = document.createElement ("div");
+        div.innerHTML = `<div class="card border m-3 p-5">
+                <div class="pro-pic">
+                    <img class="w-50" src="${player.strThumb}" alt="">
+                </div>
+                <h2>Name: ${player.strPlayer}</h2>
+                <h5>Country: ${player.strNationality}</h5>
+                <p></p>
+                <div class="allbutton">
+                    <button class="btn btn-danger">Delete</button>
+                    <button onclick="details('${player.idPlayer}')" class="btn btn-success">Details</button>
+                </div>
+            </div>`;
+        parent.appendChild(div);
+    }    
+};
+const details = (id) => {
+    const url = `https://www.thesportsdb.com/api/v1/json/2/lookupplayer.php?id=${id}`;
+    fetch(url)
+    .then((response) => response.json())
+    .then((data) => setDetails(data.players[0]));
+};
+
+const setDetails = (info) => {
+    document.getElementById("details-container").innerHTML = `
+    <div>
+        <img src="" alt="">
+        <h1>Name : ${info.strPlayer}</h1>
+    </div>
+    `;
+};
